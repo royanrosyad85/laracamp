59cf82751f131b0fceca0a07994a778d6080c752
royanrosyad85 <royan.rosyad21@mhs.uinjkt.ac.id>
2023-10-15 19:22:06 +0700

    Update integration

--- laracamp/routes/web.php
+++ laracamp/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\User\CheckoutController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,13 +23,17 @@ Route::get('login', function () {
     return view('login');
 })->name('login');
 
-Route::get('checkout', function () {
-    return view('checkout');
-})->name('checkout');
+// Route::get('checkout/{camp:slug}', function () {
+//     return view('checkout');
+// })->name('checkout');
 
-Route::get('success-checkout', function () {
-    return view('success_checkout');
-})->name('success-checkout');
+Route::get('checkout/success', [CheckoutController::class, 'success'])->name('checkout.success');
+Route::get('checkout/{camp:slug}', [CheckoutController::class, 'create'])->name('checkout.create');
+Route::post('checkout/{camp}', [CheckoutController::class, 'store'])->name('checkout.store');
+
+// Route::get('success-checkout', function () {
+//     return view('success_checkout');
+// })->name('success-checkout');
 
 // socialite routes
 Route::get('sign-in-google', [UserController::class, 'google'])->name('user.login.google');
